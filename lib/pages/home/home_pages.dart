abd99f69808c39cb00d4b3c6707bc52dcf239b7a
uits <vnfdnservice@gmail.com>
2020-07-12 16:48:48 +0700

    listener data

--- lib/pages/home/home_pages.dart
+++ lib/pages/home/home_pages.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mvvmarchitecture_flutter/base/base_viewmodel_provider.dart';
+import 'package:mvvmarchitecture_flutter/model/user_model.dart';
 import 'package:mvvmarchitecture_flutter/pages/home/home_page_viewmodel.dart';
+import 'package:mvvmarchitecture_flutter/uitls/helpers.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,16 +12,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<User> litems = [];
+
   @override
   Widget build(BuildContext context) {
-    return ViewModelProvider.withConsumer(
+    return ViewModelProvider<HomePageViewModel>.withConsumer(
         viewModelBuilder: () => HomePageViewModel("2"),
-        onModelReady: (model) => model.initialise(),
+        onModelReady: (model) => model.initialise(onLoading: () {
+              Helpers.onLoading(context);
+            }, onSuccess: () {
+              litems = model.userResponse.data;
+              print("_HomePageState   --- > ${litems.toString()}");
+            }),
         builder: (context, model, _) => Scaffold(
               appBar: AppBar(
                 title: Text('List User'),
               ),
-              body: Container(),
+              body: Container(
+                child: ListView.builder(
+                    itemCount: litems.length,
+                    itemBuilder: (BuildContext ctxt, int Index) {
+                      return new Text(litems[Index].email);
+                    }),
+              ),
             ));
   }
 }
